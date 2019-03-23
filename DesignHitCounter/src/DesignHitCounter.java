e4cfa65970f9e875870c5e8287aabbec1df2e3d4
Valkyrie <wyfwork99@gmail.com>
2019-03-23 00:58:36 -0400

    DesignHitCounter

---
+++ DesignHitCounter/src/DesignHitCounter.java
@@ -0,0 +1,35 @@
+public class DesignHitCounter {
+    int[] times;
+    int[] hits;
+
+    /** Initialize your data structure here. */
+    public DesignHitCounter() {
+        times = new int[300];
+        hits = new int[300];
+    }
+
+    /** Record a hit.
+     @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        int idx = timestamp % 300;
+        if (times[idx] != timestamp) {
+            times[idx] = timestamp;
+            hits[idx] = 1;
+        }
+        else {
+            hits[idx]++;
+        }
+    }
+
+    /** Return the number of hits in the past 5 minutes.
+     @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        int total = 0;
+        for (int i = 0; i < 300; ++i) {
+            if (timestamp - times[i] < 300) {
+                total += hits[i];
+            }
+        }
+        return total;
+    }
+}
