bccea4ee579c8d66ec0a115f547be76aed93da6b
Valkyrie <wyfwork99@gmail.com>
2019-03-23 01:14:08 -0400

    EncodeAndDecodeTinyURL

---
+++ EncodeAndDecodeTinyURL/src/EncodeAndDecodeTinyURL.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class EncodeAndDecodeTinyURL {
+    Map<Integer, String> map = new HashMap<>();
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        map.put(longUrl.hashCode(), longUrl);
+        return "http://tinyurl.com/"+longUrl.hashCode();
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(Integer.valueOf(shortUrl.replace("http://tinyurl.com/", "")));
+    }
+}
