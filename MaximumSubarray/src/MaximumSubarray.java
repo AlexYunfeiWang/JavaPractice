c35eb9cc6d493ccfd1e1b557cbb8f567dfb2972c
Valkyrie <wyfwork99@gmail.com>
2019-03-28 20:47:23 -0400

    MaximumSubarray

---
+++ MaximumSubarray/src/MaximumSubarray.java
@@ -0,0 +1,19 @@
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        int min = 0;
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; ++i) {
+            sum += nums[i];
+            max = Math.max(max, sum - min);
+            min = Math.min(min, sum);
+        }
+
+        return max;
+    }
+}
