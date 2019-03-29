8a31cd3f742066108b0a3a73932a8ee71dd5af47
Valkyrie <wyfwork99@gmail.com>
2019-03-29 18:42:34 -0400

    MinStack

---
+++ MinStack/src/MinStack.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || minStack.peek() >= x) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        int item = stack.peek();
+        if (item == minStack.peek()) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
