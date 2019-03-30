a227e1686a74f7191eeb72779fb1ceaced4a27fe
Valkyrie <wyfwork99@gmail.com>
2019-03-30 00:50:27 -0400

    PlusOne

--- PlusOne/src/PlusOne.java
+++ PlusOne/src/PlusOne.java
@@ -2,32 +2,30 @@ import java.util.Arrays;
 
 public class PlusOne {
     public int[] plusOne(int[] digits) {
-        int i = digits.length-1;
-        digits[i] += 1;
+        if (digits == null || digits.length == 0) {
+            return new int[0];
+        }
+        int carry = 0;
+        digits[digits.length-1] += 1;
 
-        int carry = digits[i]/10;
-        digits[i] %= 10;
-        i--;
-
-        while(i >= 0 && carry > 0) {
-            digits[i] += carry;
-            carry = digits[i]/10;
-            if (carry > 0) {
-                digits[i] %= 10;
-                i--;
+        for (int i = digits.length-1; i > 0; --i) {
+            if (digits[i] > 9) {
+                digits[i] = 0;
+                digits[i-1] += 1;
             }
             else {
                 break;
             }
         }
 
-        if (carry > 0) {
-            int n = digits.length;
-            digits = new int[n+1];
-            Arrays.fill(digits, 0);
-            digits[0] = 1;
+        if (digits[0] <= 9) {
+            return digits;
         }
 
-        return digits;
+        int[] res = new int[digits.length+1];
+        Arrays.fill(res, 0);
+        res[0] = 1;
+
+        return res;
     }
 }
