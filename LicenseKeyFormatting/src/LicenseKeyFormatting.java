f2c6c73cddd8829bc440134620ecb09907ba7848
Valkyrie <wyfwork99@gmail.com>
2019-04-07 17:04:12 -0400

    LicenseKeyFormatting

---
+++ LicenseKeyFormatting/src/LicenseKeyFormatting.java
@@ -0,0 +1,21 @@
+public class LicenseKeyFormatting {
+    public String licenseKeyFormatting(String S, int K) {
+        StringBuilder sb = new StringBuilder();
+        int dashes = 0;
+
+        for (int i = S.length()-1; i >= 0; --i) {
+            char c = S.charAt(i);
+            if (c == '-') {
+                continue;
+            }
+            sb.append(c);
+            if ((sb.length() - dashes)%K == 0) {
+                sb.append('-');
+                dashes++;
+            }
+        }
+        if (sb.length() > 0 && sb.charAt(sb.length()-1) == '-')
+            sb.deleteCharAt(sb.length()-1);
+        return sb.reverse().toString().toUpperCase();
+    }
+}
